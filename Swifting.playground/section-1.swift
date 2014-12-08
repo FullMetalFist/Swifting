a3fcc90c7990e06be1e259bc44c67968d06308f8
Michael Vilabrera <m.vilabrera@gmail.com>
2014-12-08 13:48:59 -0500

    Added classes and enums

--- Swifting.playground/section-1.swift
+++ Swifting.playground/section-1.swift
@@ -192,3 +192,175 @@ numbers.map({
         return 0
     }
 })
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name:String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "exampleSquare")
+test.area()
+test.simpleDescription()
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "theTriangle")
+triangle.perimeter
+triangle.perimeter = 9.9
+triangle.sideLength
+
+class TriangleAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    var square: Square {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+triangleAndSquare.square.sideLength
+triangleAndSquare.triangle.sideLength
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+triangleAndSquare.triangle.sideLength
+triangleAndSquare.square.sideLength
+
+class Counter {
+    var count: Int = 0
+    func incrementBy(amount: Int, numberOfTimes times:Int) {
+        count += amount * times
+    }
+}
+var counter = Counter()
+counter.incrementBy(2, numberOfTimes: 7)
+
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.Ace
+let aceRawValue = ace.rawValue
+
+func compareRank(firstVal: Rank, secondVal: Rank) -> String {
+    if (firstVal.rawValue == secondVal.rawValue) {
+        return "values are equal"
+    } else if (firstVal.rawValue > secondVal.rawValue) {
+        return "first value is greater"
+    } else {
+        return "second value is greater"
+    }
+}
+
+var card1 = Rank.King
+var card2 = Rank.Jack
+var comarisonResult = compareRank(card1, card2)
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+}
+
+enum Suit {
+    case Spades, Hearts, Diamonds, Clubs
+    func simpleDescription() -> String {
+        switch self {
+        case .Spades:
+            return "spades"
+        case .Hearts:
+            return "hearts"
+        case .Diamonds:
+            return "diamonds"
+        case .Clubs:
+            return "clubs"
+        }
+    }
+    func color() -> String {
+        switch self {
+        case .Spades .Clubs;:
+            return "black"
+        case .Hearts .Diamonds;:
+            return "red"
+        }
+    }
+}
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
